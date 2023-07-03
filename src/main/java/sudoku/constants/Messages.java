127de349ab8f5cd51a04209b10a128a2b4e17c38
MuhammadTaha <sohailtaha123@gmail.com>
2023-07-03 14:56:23 +0500

    Problem Domain Classes and Constants done

---
+++ src/main/java/sudoku/constants/Messages.java
@@ -0,0 +1,7 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "CONGRATULATIONS, My Dear you have WON!!! New Game?";
+    public static final String ERROR = "An error has occurred. ;(";
+
+}
--- src/main/java/sudoku/problemdomain/SudokuGame.java
+++ src/main/java/sudoku/problemdomain/SudokuGame.java
@@ -1,4 +1,23 @@
 package sudoku.problemdomain;
 
-public class SudokuGame {
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+    private final GameState gameState;
+    private final int[][] gridState;
+
+    public static final int  GRID_BOUNDARY = 9;
+
+    public SudokuGame(GameState gameState, int[][] gridState) {
+        this.gameState = gameState;
+        this.gridState = gridState;
+    }
+
+    public GameState getGameState() {
+        return gameState;
+    }
+
+    public int[][] getCopyofGridState() {
+        return SudokuUtilities.copyToNewArray(gridState);
+    }
 }
